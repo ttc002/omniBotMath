fd151edfc410c7fdcee020c8bc3b372008d8ea5a
ttc002 <shokalPetr@gmail.com>
2024-10-25 10:53:14 +0000

    bugfix

--- rot_count.py
+++ rot_count.py
@@ -3,11 +3,14 @@ wheel_diametr = 10
 def count_main_wheel(speed, ang):
     global wheel_diametr
     tang_vel = math.degrees(speed/math.cos(math.pi/3 - math.radians(ang)))
-    return math.degrees((1000*tang_vel)/(math.pi*wheel_diametr))
+    return math.degrees((1*tang_vel)/(math.pi*wheel_diametr))
 
 def count_help_wheel(speed,ang):
     global wheel_diametr
     numerator = speed*math.degrees(math.sin(math.pi/3 - math.radians(ang)))
-    denumerator = math.degrees(math.cos(math.pi/3-ang)) * math.degrees(math.cos(math.pi/6 - ang))
-    return math.degrees((1000*(numerator/denumerator))/(math.pi*wheel_diametr))
+    denumerator = math.degrees(math.cos(math.pi/3-math.radians(ang))) * math.degrees(math.cos(math.pi/6 - math.radians(ang)))
+    return math.degrees((1 * (numerator/denumerator))/(math.pi*wheel_diametr))
 
+ang = 0
+speed = 1 # m/s
+print(count_main_wheel(speed,ang))
